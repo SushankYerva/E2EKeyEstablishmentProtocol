21f33058cba7e4e019c87874036fb3c6d8f1a0d7
SushankYerva <ysushank@gmail.com>
2025-10-22 15:15:19 +0100

    updated tests

--- tests/test_protocol_sanity.py
+++ tests/test_protocol_sanity.py
@@ -7,7 +7,7 @@ import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
 
 def test_modules_import():
-    for mod in ("src.server", "src.client", "src.certificates"):
+    for mod in ("server", "client", "certificates"):
         importlib.import_module(mod)
 
 def test_cert_api_shape():
